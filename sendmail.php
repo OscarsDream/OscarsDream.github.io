8a11fee0c9e3d7ef1cf1206e0121290fb1af6963
OscarsDream <ot.coyote@proton.me>
2025-07-28 18:13:19 -0700

    Add files via upload
    Adding files created by chatGPT to let users send email to civara.us
    new file:  sendmail.php
    updated file:  index.html

---
+++ sendmail.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "you@civara.us"; // Replace with your actual email
+    $subject = isset($_POST['newsletter']) ? "New Newsletter Signup" : "New Contact Form Submission";
+
+    $email = strip_tags($_POST["email"]);
+    $name = isset($_POST["name"]) ? strip_tags($_POST["name"]) : "N/A";
+    $message = isset($_POST["message"]) ? strip_tags($_POST["message"]) : "N/A";
+
+    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully.";
+    } else {
+        echo "Error sending message.";
+    }
+}
+?>
